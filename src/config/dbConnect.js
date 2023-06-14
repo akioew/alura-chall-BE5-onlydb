6f3f065e5b71a8411efefb127870c74956368a09
Ewerton <104736428+akioew@users.noreply.github.com>
2023-06-14 15:13:39 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  'mongodb+srv://***REMOVED***'
+  //'mongodb+srv://XXXXX' Inserir dados de conexão com o Banco de Dados
 );
 
 let db = mongoose.connection;
